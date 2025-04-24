5c80db6ceb66b52664d08ca4f086f6b63886f5e1
ranzdbg <ranzdenzel.bautista@plazacastilla.salesianas.org>
2025-04-24 12:11:18 +0200

    Add files via upload

---
+++ Práctica1/src/práctica1/Ejercicio1.java
@@ -0,0 +1,13 @@
+package práctica1;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*System.out.println("Me gusta la programación");
+		System.out.println("Cada día");
+		System.out.println("MASSS");*/
+		System.out.println("Me gusta la programación\nCada día\nMASSS");
+	}
+
+}
