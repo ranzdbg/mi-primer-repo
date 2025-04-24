d2fbdae329de5da748d187a767d21fa4e3aca0c9
ranzdbg <ranzdenzel.bautista@plazacastilla.salesianas.org>
2025-04-24 12:14:14 +0200

    Update Ejercicio1.java

--- Práctica1/src/práctica1/Ejercicio1.java
+++ Práctica1/src/práctica1/Ejercicio1.java
@@ -1,7 +1,7 @@
 package práctica1;
 
 public class Ejercicio1 {
-
+	// Estoy haciendo la práctica 20
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		/*System.out.println("Me gusta la programación");
